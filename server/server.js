b9fc0102601579e52f4cf76fd44963a233f90c14
Carlos Flores <caflores1094@gmail.com>
2017-01-11 20:21:55 -0800

    Change to port 80

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ require('./routes.js');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80; 
 
 app.listen(port, () => {
   console.log('App listening on port ' + port);
